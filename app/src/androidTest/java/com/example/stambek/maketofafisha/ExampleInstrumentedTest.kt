cbdaa593731ed2ceedc8981e9d73ca8f3d8f2cb3
Dosya <alybaev_d@auca.kg>
2018-06-28 18:02:41 +0600

    I fixed the error(Perform Exception),adding Thread.sleep

--- app/src/androidTest/java/com/example/stambek/maketofafisha/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/stambek/maketofafisha/ExampleInstrumentedTest.kt
@@ -1,31 +1,27 @@
 package com.example.stambek.maketofafisha
 
-import android.support.test.espresso.Espresso
 import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.IdlingRegistry
-import android.support.test.espresso.IdlingResource
+import android.support.test.espresso.action.ViewActions
 import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.assertion.ViewAssertions.matches
-import android.support.test.espresso.contrib.RecyclerViewActions
+import android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import android.support.test.espresso.contrib.RecyclerViewActions.scrollToPosition
 import android.support.test.espresso.intent.Intents
-import android.support.test.espresso.intent.Intents.intended
-import android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
-import android.support.test.runner.lifecycle.ActivityLifecycleMonitor
-import android.support.test.runner.lifecycle.ActivityLifecycleMonitorRegistry
-import android.support.test.runner.lifecycle.Stage
 import android.support.v7.widget.RecyclerView
-import android.util.Log
-import com.example.stambek.maketofafisha.R.id.*
+import com.example.stambek.maketofafisha.R.id.list_view
+import com.example.stambek.maketofafisha.R.id.list_view2
 import com.example.stambek.maketofafisha.ui.cinema.MainActivity
 import com.example.stambek.maketofafisha.ui.movie.SecondActivity
+import kotlinx.android.synthetic.main.activity_second.*
 import kotlinx.android.synthetic.main.activity_shedule_.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.*
 
 
 /**
@@ -35,122 +31,35 @@ import org.junit.runner.RunWith
  */
 @RunWith(AndroidJUnit4::class)
 class CinemaAppTest {
-
     @get:Rule
     val mActivityRule = ActivityTestRule(
             MainActivity::class.java)
 
     @Test
     fun checkWorkOfAfisha() {
+        Thread.sleep(1000)
+        var rnd = Random()
+        var itemsCinema = mActivityRule.activity.list_view.adapter.itemCount
+        var casualNum = rnd.nextInt(itemsCinema)
 
-        val recyclerView = mActivityRule.activity.list_view
-        val itemCount = recyclerView.adapter.itemCount
 
-        Log.d("Item count is ", itemCount.toString())
+        onView(withId(list_view)).perform(scrollToPosition<RecyclerView.ViewHolder>(casualNum))
+        Thread.sleep(3000)
+        onView(withId(list_view)).perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(casualNum, click()))
+        Thread.sleep(3000)
 
-        // Scroll to end of page with position in Recycle view 1
-        scrollAndClickRecycle(itemCount)
+        onView(withId(list_view2)).check(matches(isDisplayed()))
+        onView(withId(list_view2)).perform(scrollToPosition<RecyclerView.ViewHolder>(0))
+        onView(withId(list_view2)).perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        Thread.sleep(3000)
+        onView(withId((R.id.imageView))).check(matches(isDisplayed()))
+        onView(withId((R.id.information))).check(matches(isDisplayed()))
 
-        //check was Second Activity launched
-        val secondActivityName = SecondActivity::class.java!!.name
-        IdlingRegistry.getInstance().register(WaitActivityIsResumedIdlingResource(secondActivityName))
-        intended(hasComponent(SecondActivity::class.java!!.name))
-        scrollAndClickRecycle(itemCount)
-        // Scroll to end of page with position in Recycle view 2
-
-        onView(withId(R.id.list_view2))
-                .perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(itemCount - 1))
-        // Perform click on definite view in Recycle view 2
-        onView(withId(R.id.list_view2))
-                .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(itemCount - 1, click()))
-        // is Displayed Text and Image of movie
-        onView(withId(imageView)).check(matches(isDisplayed()))
-        onView(withId(information)).check(matches(isDisplayed()))
-        //Back Button
-        Espresso.pressBack()
-        Espresso.pressBack()
+        onView(isRoot()).perform(ViewActions.pressBack())
+        onView(isRoot()).perform(ViewActions.pressBack())
+        Thread.sleep(3000)
+        onView(withId(list_view)).check(matches(isDisplayed()))
 
     }
-}
-fun scrollAndClickRecycle(itemCount: Int) {
-    onView(withId(list_view))
-            .perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(itemCount - 1))
-
-    // Perform click on definite view in Recycle view 1
-
-    onView(withId(list_view))
-            .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(itemCount - 1, click()))
-
-
-
-}
-//Wait Class
-private class WaitActivityIsResumedIdlingResource(private val activityToWaitClassName: String) : IdlingResource {
-
-    private val instance: ActivityLifecycleMonitor
-    @Volatile private var resourceCallback: IdlingResource.ResourceCallback? = null
-    internal var resumed = false
-
-    private val isActivityLaunched: Boolean
-        get() {
-            val activitiesInStage = instance.getActivitiesInStage(Stage.RESUMED)
-            for (activity in activitiesInStage) {
-                if (activity.javaClass.name == activityToWaitClassName) {
-                    return true
-                }
-            }
-            return false
-        }
-
-    init {
-        instance = ActivityLifecycleMonitorRegistry.getInstance()
-    }
-
-    override fun getName(): String {
-        return this.javaClass.name
-    }
-
-    override fun isIdleNow(): Boolean {
-        resumed = isActivityLaunched
-        if (resumed && resourceCallback != null) {
-            resourceCallback!!.onTransitionToIdle()
-        }
-
-        return resumed
-    }
-
-    override fun registerIdleTransitionCallback(resourceCallback: IdlingResource.ResourceCallback) {
-        this.resourceCallback = resourceCallback
-    }
-
-}
-
-class ElapsedTimeIdlingResource(private val waitingTime: Long) : IdlingResource {
-
-    private val startTime: Long
-    private var resourceCallback: IdlingResource.ResourceCallback? = null
-
-    init {
-        this.startTime = System.currentTimeMillis()
-    }
-
-    override fun getName(): String {
-        return ElapsedTimeIdlingResource::class.java.name + ":" + waitingTime
-    }
-
-    override fun isIdleNow(): Boolean {
-        val elapsed = System.currentTimeMillis() - startTime
-        val idle = elapsed >= waitingTime
-        if (idle) {
-            resourceCallback!!.onTransitionToIdle()
-        }
-        return idle
-    }
-
-    override fun registerIdleTransitionCallback(
-            resourceCallback: IdlingResource.ResourceCallback) {
-        this.resourceCallback = resourceCallback
-    }
-
 }
 
